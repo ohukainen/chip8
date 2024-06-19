65d8a80c60a1c1d4b7b382d5e38a804ae632f178
Ohukainen <153552214+Ohukainen@users.noreply.github.com>
2024-06-19 17:10:30 +0200

    #2 Code skeleton with TODO comments added

--- include/chip8.hpp
+++ include/chip8.hpp
@@ -1,10 +1,37 @@
 #pragma once
 
 #include <cstdint>
+#include <array>
+#include <string>
 
 using Byte = uint8_t;
+using Word = uint16_t;
 
 class Chip8 {
 public:
     void initialize();   
+    void loadGame(const std::string& gameFilepath);
+
+    void emulateCycle();
+    void setKeys(const std::array<Byte, 16>& keyState);
+
+    bool getDrawFlag() const;
+private:
+    std::array<Byte, 4096> mMemory;     // Memory 
+    std::array<Byte, 64 * 32> mGraphix; // Pixels on the screen 
+
+    Word mOpcode;                       // Operation code 
+    Word mProgramCounter;               // Program counter 
+    Word mIndexRegistry;                // Index regisgry 
+    std::array<Byte, 16> mVReg;         // V registry
+
+    Byte mDelayTimer;                   // Timer for delay
+    Byte mSoundTimer;                   // Timer for sound      
+                            
+    std::array<Word, 16> mStack;        // Stack 
+    Word mStackP;                       // Stack pointer
+
+    std::array<Byte, 16> mKeys;         // Keypad representation
+
+    bool mDrawFlag;                     // Flag for refreshing screen
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
+#include <SDL.h>
 #include <chip8.hpp>
+#include <iostream>
 
-int main() {
+int main(int argc, char** argv) {
     Chip8 chip8;
+    bool running = true;
+
+    // TODO: set up graphics system with SDL 
+    // TODO: setup keyboard input handling 
+
+    if (argc != 1) {
+        std::cout << "The program don't support input right now, please don't provide any" << std::endl;
+        return 0;
+    }
+    std::cout << "Started emulator: " << argv[0] << std::endl;
 
     chip8.initialize();
+    
+    // TODO: LoadGame with reference to input
+    // chip8.loadGame("game.rom");
+    
+    while (running) {
+        chip8.emulateCycle();
+
+        if (chip8.getDrawFlag()) {
+            // TODO: Update the screen
+        }
+       
+        //TODO: get keyState from input handling and notify chip8
+        // chip8.setKeys(keyState);
+        running = false;
+    }
+    std::cin.get();
+    return 0;
 }
